866acf640499748fd9ca28b77de5755871281e60
Cody <Cojo341@gmail.com>
2022-08-05 14:58:40 -0400

    Finished ToDos

--- Variables Exercise 01/Program.cs
+++ Variables Exercise 01/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 
 namespace Variables_Exercise_01
 {
@@ -10,14 +10,19 @@ namespace Variables_Exercise_01
             // - Once finished save, commit, and then push to GitHub!
 
             // TODO: Declare 3 constansts
+            const string how = "I am";
+            const string are = " doing just";
+            const string you = " fine thank you.";
+
 
             #region Types
-            // string
-            // char
-            // bool
-            // int
-            // double
-            // decimal
+            string School = "TrueCoders";
+            char myLang = 'C';
+            int myClass = 29;
+            double classStart = 10;
+            decimal classEnd = 4;
+            bool enroll;
+
             #endregion
 
             // TODO: Declare a variable for each of the types above
@@ -26,7 +31,41 @@ namespace Variables_Exercise_01
             // TODO: Initialize each of the variables with a value
 
             // TODO: Write out each of the variables to the Console 
-        }
+            Console.WriteLine("Hello how are you?");
 
+            Console.WriteLine(how + are + you);
+
+            Console.WriteLine($"You are in {School} class {myLang}-{myClass} Correct?");
+
+            Console.WriteLine("Please use True/False: ");
+
+            var inputString = Console.ReadLine();
+            if (inputString == "True")
+            {
+                enroll = true;
+            }
+            else if (inputString == "False")
+            {
+                enroll = false;
+            }
+            else
+            {
+                enroll = false;
+                Console.WriteLine("Please use True/False: ");
+            }
+
+            if (enroll == true)
+            {
+                Console.WriteLine($"Great! Class starts at {classStart}a and ends at {classEnd}p");
+            }
+            else if (enroll == false)
+            {
+                Console.WriteLine("Thats alright theres plenty of time to sign up!");
+            }
+            else
+            {
+                
+            }
+        }
     }
 }
